9a2f5a778a99052f4b6ae43a242b19aa131e4fac
EpicDoomSir <eitands@gmail.com>
2020-12-30 16:44:05 -0500

    logout

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,23 +1,34 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
-function Navbar() {
-    return(
-        <ul>
-            <NavLink to="/home" >
-                <li>Home</li>
-            </NavLink>
-            <NavLink to="/games" >
-                <li>Games</li>
-            </NavLink>
-            <NavLink to="/signup" >
-                <li>Sign Up</li>
-            </NavLink>
-            <NavLink to="/login" >
-                <li>Log in</li>
-            </NavLink>
-        </ul>
-    )
+class Navbar extends React.Component {
+    render(){
+        return(
+            <ul>
+                <NavLink to="/home" >
+                    <li>Home</li>
+                </NavLink>
+                <NavLink to="/games" >
+                    <li>Games</li>
+                </NavLink>
+                <NavLink to="/signup" >
+                    <li>Sign Up</li>
+                </NavLink>
+                {this.props.user ? 
+                    <li onClick={()=> {localStorage.removeItem("token")}} >Log Out</li>
+                :
+                    <NavLink to="/login" >
+                        <li>Log in</li>
+                    </NavLink>
+                }
+            </ul>
+        )
+    }
 }
 
-export default Navbar
+function msp(state){
+    return { user: state.user }
+}
+
+export default connect(msp, null)(Navbar)
